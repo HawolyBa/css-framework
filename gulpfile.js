f37b79c192aee0765b82b562c1073bea0f2d3db4
Hawoly Ba <hawolyba@gmail.com>
2018-09-30 03:17:53 +0200

    Project setup

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create();
+
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      server: "./"
+  });
+
+  gulp.watch("./sass/*.scss", ['sass']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("./sass/*.scss")
+      .pipe(sass())
+      .pipe(autoprefixer('last 2 versions'))
+      .pipe(gulp.dest("./css"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
